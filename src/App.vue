46206a3d4003d0524ca929ac40a6d0f3081cdd62
Yann <yann.popov@hotmail.fr>
2026-03-12 22:34:22 +0100

    ajout de nouvelle syntaxe vue js

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,47 @@
 <script setup>
 import { ref } from 'vue';
 
-const count = ref(0)
+// const count = ref(0)
+// const movieName = ref('')
+// const movies = ref([
+//   'matrix',
+//   'lilo',
+//   'titanic'
+// ])
 
-const increment = () => {
-  count.value++
+// const sortMovies = () =>{
+//   -movies.value.sort((a,b)=> a>b ? 1 : -1)
+// }
+
+
+// const deleteMovie = (movie) =>{
+//   movies.value = movies.value.filter(m => m !== movie)
+// }
+
+// const addMovie = () => {
+//   movies.value.push(movieName.value)
+//   movieName.value = ''
+
+// }
+
+// const increment = () => {
+//   count.value++
+// }
+
+// const decrement = () => {
+//   count.value--
+// }
+
+const person = ref({
+  firstname : 'Yann',
+  lastname: 'Popov',
+  age: 20
+})
+
+const randomAge = () => {
+  person.value.age = Math.round(Math.random()* 100)
 }
 
-const decrement = () => {
-  count.value--
-}
 
 </script>
 
@@ -17,12 +49,34 @@ const decrement = () => {
 
 
 <template>
-  <p :id="'p-${count}'"> Coucou {{ count }}</p>
+  <!-- <p :id="'p-${count}'"> Coucou {{ count }}</p>
   <button @click="increment"> Incrementer </button>
   <button @click="decrement"> Decrementer </button>
+  <hr>
+  <button @click="sortMovies">réorganiser</button>
 
-  <div v-show="count >= 5"> Vous avez cliqué plus de 5 fois </div>
+  <form action="" @submit.prevent="addMovie">
+    <input type="text" placeholder="nv film" v-model="movieName">
+    <button> Ajouter </button>
+  </form>
 
+  <ul>
+    <li 
+    v-for="movie in movies"
+    :key="movie"
+    >
+      {{ movie }} <button @click="deleteMovie(movie)"> suppr </button>
+    </li>
+  </ul>
+
+  <div v-show="count >= 5"> Vous avez cliqué plus de 5 fois </div> -->
+
+<ul>
+  <li>{{person.firstname}}</li>
+  <li>{{person.lastname}}</li>
+  <li>{{person.age}}</li>
+</ul>
+<button @click.prevent="randomAge">changer age</button>
 </template>
 
 
