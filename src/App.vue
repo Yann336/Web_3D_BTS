ff73104547c6873dbcd1deaa4dfbafe7d2d1833e
Yann <yann.popov@hotmail.fr>
2026-03-30 14:17:17 +0200

    configuration de ESlint et correction

--- src/App.vue
+++ src/App.vue
@@ -1,102 +1,102 @@
 <script setup>
 import { ref, computed } from 'vue';
 
-
-const title =ref ("");
+const title = ref('');
 const completed = ref(false);
 const date = ref();
-let id = 1
-const hide = ref(false)
+let id = 1;
+const hide = ref(false);
 
-const todolist = ref([])
+const todolist = ref([]);
 
-const addTodo = () =>{
-   todolist.value.push({
-    id : id++,
+const addTodo = () => {
+  todolist.value.push({
+    id: id += 1,
     title: title.value,
     date: date.value,
-    completed : completed.value
-   })
-  title.value = ''
-  date.value = ''
-  completed.value = false
+    completed: completed.value,
+  });
+  title.value = '';
+  date.value = '';
+  completed.value = false;
 };
 
-const deleteTodo = (id) =>{
-  todolist.value = todolist.value.filter(todo => todo.id !== id)
+const deleteTodo = (todoID) => {
+  todolist.value = todolist.value.filter((todo) => todo.id !== todoID);
 };
 
-const sortedTodos = computed(() =>{
-  let sortedTodos = todolist.value.toSorted((a,b)=> a.completed > b.completed ? 1 : -1)
-  if (hide.value){
-    sortedTodos = sortedTodos.filter(t => !t.completed );
+const sortedTodos = computed(() => {
+  let sorted = todolist.value.toSorted((a, b) => (a.completed > b.completed ? 1 : -1));
+  if (hide.value) {
+    sorted = sorted.filter((t) => !t.completed);
   }
-  return sortedTodos;
-})
+  return sorted;
+});
 
-
-const remainingTodo = computed(() => {
-  return todolist.value.filter(t => t.completed === false).length
-}
-)
+const remainingTodo = computed(() => todolist.value.filter((t) => t.completed === false).length);
 
 </script>
 
-
-
-
 <template>
-
-<form action="" @submit.prevent="addTodo">
-  <input type="text" placeholder="Nom" v-model="title">
-  <br>
-  <input type="date" placeholder="date" v-model = "date">
-  <br>
-  <button :disabled="title.length === 0"> Ajouter </button>
-</form>
-
-
-
-<ul>
-  <p v-if="todolist.length === 0"> Ajouter des taches</p>
-
-    <li v-for="todo in sortedTodos"
-    :key="todo.id" 
-    :class="{barré: todo.completed }" 
+  <form
+    action=""
+    @submit.prevent="addTodo"
+  >
+    <input
+      v-model="title"
+      type="text"
+      placeholder="Nom"
     >
-    
+    <br>
+    <input
+      v-model="date"
+      type="date"
+      placeholder="date"
+    >
+    <br>
+    <button :disabled="title.length === 0">
+      Ajouter
+    </button>
+  </form>
+
+  <ul>
+    <p v-if="todolist.length === 0">
+      Ajouter des taches
+    </p>
+
+    <li
+      v-for="todo in sortedTodos"
+      :key="todo.id"
+      :class="{barré: todo.completed }"
+    >
+      <label>
+        <input
+          v-model="todo.completed"
+          type="checkbox"
+        >
+        {{ todo.title }} {{ todo.date }}
+
+        <button @click="deleteTodo(todo.id)"> Supprimer </button>
+      </label>
+    </li>
+  </ul>
+
+  <div>
     <label>
-      <input type="checkbox" v-model="todo.completed">
-      {{ todo.title }} {{ todo.date }} 
-
-      <button @click="deleteTodo(todo.id)"> Supprimer </button>
+      <input
+        v-model="hide"
+        type="checkbox"
+      >
+      Masquer les taches complétées
     </label>
-  </li>
-</ul>
-
-
-<div>
-  <label>
-    <input type="checkbox" v-model="hide">
-    Masquer les taches complétées
-  </label>
-  <p v-if="remainingTodo > 0">
-    {{ remainingTodo }} tâche{{  remainingTodo > 1 ? 's' : '' }} à faire
-  </p>
-</div>
-
-
-
-
+    <p v-if="remainingTodo > 0">
+      {{ remainingTodo }} tâche{{ remainingTodo > 1 ? 's' : '' }} à faire
+    </p>
+  </div>
 </template>
-
-
 
 <style>
   .barré{
     text-decoration: line-through;
   }
 </style>
-
-
-
--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -6,80 +6,90 @@ const movieName = ref('');
 const movies = ref([
   'matrix',
   'lilo',
-  'titanic'
+  'titanic',
 ]);
 
-const sortMovies = () =>{
-  movies.value.sort((a,b)=> a>b ? 1 : -1)
+const sortMovies = () => {
+  movies.value.sort((a, b) => (a > b ? 1 : -1));
 };
 
-
-const deleteMovie = (movie) =>{
-  movies.value = movies.value.filter(m => m !== movie)
+const deleteMovie = (movie) => {
+  movies.value = movies.value.filter((m) => m !== movie);
 };
 
 const addMovie = () => {
-  movies.value.push(movieName.value)
-  movieName.value = ''
-
+  movies.value.push(movieName.value);
+  movieName.value = '';
 };
 
 const increment = () => {
-  count.value++
+  count.value += 1;
 };
 
 const decrement = () => {
-  count.value--
+  count.value -= 1;
 };
 
-
-
-
 const person = ref({
-  firstname : 'Yann',
+  firstname: 'Yann',
   lastname: 'Popov',
-  age: 20
+  age: 20,
 });
 
 const randomAge = () => {
-  person.value.age = Math.round(Math.random()* 100)
-}
+  person.value.age = Math.round(Math.random() * 100);
+};
 
 </script>
 
-
-
-
 <template>
-    
-
-  <p :id="'p-${count}'"> Coucou {{ count }}</p>
-  <button @click="increment"> Incrementer </button>
-  <button @click="decrement"> Decrementer </button>
+  <p :id="'p-${count}'">
+    Coucou {{ count }}
+  </p>
+  <button @click="increment">
+    Incrementer
+  </button>
+  <button @click="decrement">
+    Decrementer
+  </button>
   <hr>
-  <button @click="sortMovies">réorganiser</button>
+  <button @click="sortMovies">
+    réorganiser
+  </button>
 
-  <form action="" @submit.prevent="addMovie">
-    <input type="text" placeholder="nv film" v-model="movieName">
+  <form
+    action=""
+    @submit.prevent="addMovie"
+  >
+    <input
+      v-model="movieName"
+      type="text"
+      placeholder="nv film"
+    >
     <button> Ajouter </button>
   </form>
 
   <ul>
-    <li 
-    v-for="movie in movies"
-    :key="movie"
+    <li
+      v-for="movie in movies"
+      :key="movie"
     >
-      {{ movie }} <button @click="deleteMovie(movie)"> suppr </button>
+      {{ movie }} <button @click="deleteMovie(movie)">
+        suppr
+      </button>
     </li>
   </ul>
 
-  <div v-show="count >= 5"> Vous avez cliqué plus de 5 fois </div> 
+  <div v-show="count >= 5">
+    Vous avez cliqué plus de 5 fois
+  </div>
 
-<ul>
-  <li>{{person.firstname}}</li>
-  <li>{{person.lastname}}</li>
-  <li>{{person.age}}</li>
-</ul>
-<button @click.prevent="randomAge">changer age</button>
-
+  <ul>
+    <li>{{ person.firstname }}</li>
+    <li>{{ person.lastname }}</li>
+    <li>{{ person.age }}</li>
+  </ul>
+  <button @click.prevent="randomAge">
+    changer age
+  </button>
 </template>
