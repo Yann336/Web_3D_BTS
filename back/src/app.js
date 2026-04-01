cfe1bcdbbcc0063b7dba5f6afdb5f3635d129004
Yann <yann.popov@hotmail.fr>
2026-04-01 10:18:08 +0200

    Réorganisation de l'architecture du projet

---
+++ back/src/app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port ${PORT}`);
+});
+
+export default app;
