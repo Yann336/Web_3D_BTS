8ddf136f17005fbcedf65f93d2f96867bf3c47f5
Yann <yann.popov@hotmail.fr>
2026-04-03 22:37:45 +0200

    ajout de nodemon

--- back/app.js
+++ back/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
-import swaggerSpec from './config/swagger';
+import swaggerSpec from './src/config/swagger';
 
 const app = express(); // La création de l'app
 const PORT = 3000; // numéro de port sur lequel le serveur va écouter.
