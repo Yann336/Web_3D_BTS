1ee8de7da083e22384bbd36f1fe36d6958374c26
Yann <yann.popov@hotmail.fr>
2026-04-29 23:19:40 +0200

    ajout de la page description d'un produit séelctionné création de la route et affichage dynamique

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -3,12 +3,15 @@ import Home from '../views/home.vue';
 import Connection from '../views/connection.vue';
 import Basket from '../views/Basket.vue';
 import Printing from '../views/3dPrinting.vue';
+import Product from '../views/product.vue';
 
 const routes = [
   { path: '/', component: Home },
   { path: '/connection', component: Connection },
   { path: '/basket', component: Basket },
   { path: '/3d-printing', component: Printing },
+  { path: '/product/:id', component: Product },
+
 ];
 
 const router = createRouter({
--- front/src/views/3dPrinting.vue
+++ front/src/views/3dPrinting.vue
@@ -11,7 +11,7 @@ const error = ref(null);
 onMounted(async () => {
   try {
     // envoie requête GET à API Express, await met en pause le code jusqu'à ce qu'Express réponde
-    const response = await fetch('http://localhost:3000/api/products');
+    const response = await fetch('http://localhost:3000/api/products/');
     // response.ok est true si Express a répondu
     if (!response.ok) {
       throw new Error('Erreur lors de la récupération des produits');
@@ -27,28 +27,31 @@ onMounted(async () => {
 </script>
 
 <template>
-  <p v-if="loading">
-    Chargements des produits...
-  </p>
-  <p v-else-if="error">
-    {{ error }}
-  </p>
-  <div
-    v-else
-    class="products-flex"
-  >
+  <div>
+    <p v-if="loading">
+      Chargements des produits...
+    </p>
+    <p v-else-if="error">
+      {{ error }}
+    </p>
     <div
-      v-for="product in products"
-      :key="product.id"
-      class="product-card"
+      v-else
+      class="products-flex"
     >
-      <img
-        :src="`/img/products/${product.image_url}`"
-        alt=""
-        class="product-img"
+      <RouterLink
+        v-for="product in products"
+        :key="product.id"
+        class="product-card"
+        :to="`/product/${product.id}`"
       >
-      <h3>{{ product.name }}</h3>
-      <p>{{ product.price }} €</p>
+        <img
+          :src="`/img/products/${product.image_url}`"
+          alt=""
+          class="product-img"
+        >
+        <h3>{{ product.name }}</h3>
+        <p>{{ product.price }} €</p>
+      </RouterLink>
     </div>
   </div>
 </template>
